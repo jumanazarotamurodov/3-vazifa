8ecc9df5b59dd21bc8af4e1fd89c8b5957c56ba1
jumanazarotamurodov <jotamurodov8@gmail.com>
2023-04-12 15:59:08 +0500

    reset

--- app.js
+++ app.js
@@ -6,18 +6,22 @@ function tes1(){
     a++
 
     h.textContent = a
-    h.style.color = "green"
-    if(h.textContent>9){
-    h.style.color = "blue"
+    h.style.color = "black"
+    if(h.textContent<0){
+        h.style.color = "red"
+    }else if(h.textContent>9){
+        h.style.color = "blue"
     }
 }
 
 function tes2(){
     a--
-    h.style.color = "green"
+    h.style.color = "black"
     h.textContent = a
     if(h.textContent<0){
         h.style.color = "red"
+    }else if(h.textContent>9){
+        h.style.color = "blue"
     }
 }
 
